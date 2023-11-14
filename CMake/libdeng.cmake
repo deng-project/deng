d5a4e806829683adc2f855632793fb7ff398eb02
inugami-dev64 <karl@karlott.net>
2023-11-15 01:48:56 +0200

    Add source filters in libdeng.cmake file

--- CMake/libdeng.cmake
+++ CMake/libdeng.cmake
@@ -5,100 +5,173 @@
 	
 ## Minimal debug build with only Vulkan renderer
 set(DENG_MINIMAL_TARGET deng-minimal)
+
+# ImGui sources
 set(DENG_IMGUI_HEADERS 
 	ThirdParty/imgui/imgui.h
 	ThirdParty/imgui/imconfig.h
 	ThirdParty/imgui/imgui_internal.h
 	ThirdParty/imgui/imstb_rectpack.h
 	ThirdParty/imgui/imstb_textedit.h
-	ThirdParty/imgui/imstb_truetype.h)
+	ThirdParty/imgui/imstb_truetype.h
+	Include/deng/ImGuiLayer.h
+	Include/deng/ImGuiResourceBuilders.h
+)
 	
 set(DENG_IMGUI_SOURCES
 	ThirdParty/imgui/imgui.cpp
     ThirdParty/imgui/imgui_demo.cpp
     ThirdParty/imgui/imgui_draw.cpp
     ThirdParty/imgui/imgui_tables.cpp
-    ThirdParty/imgui/imgui_widgets.cpp)
-	
-set(DENG_MINIMAL_HEADERS
-	Include/deng/Api.h
-	Include/deng/App.h
-	Include/deng/CameraTransformer.h
-	Include/deng/Components.h
-	Include/deng/ErrorDefinitions.h
-	Include/deng/Event.h
-	Include/deng/Exceptions.h
-	Include/deng/FileTextureBuilder.h
-	Include/deng/FileSystemShader.h
-	Include/deng/GPUMemoryAllocator.h
-	Include/deng/HLSLGraphicsShaderSpirvCompiler.h
-	Include/deng/IFramebuffer.h
-	Include/deng/ILayer.h
-	Include/deng/ImGuiLayer.h
-	Include/deng/ImGuiResourceBuilders.h
-	Include/deng/InitializerListBuilder.h
-	Include/deng/InputEvents.h
+    ThirdParty/imgui/imgui_widgets.cpp
+	Sources/ImGuiLayer.cpp
+	Sources/ImGuiResourceBuilders.cpp
+)
+
+# Graphics backend sources
+set(DENG_RENDERER_BACKEND_HEADERS 
 	Include/deng/IRenderer.h
-	Include/deng/IShader.h
-	Include/deng/IGraphicsShaderSpirvCompiler.h
-	Include/deng/IWindowContext.h
-	Include/deng/MathConstants.h
-	Include/deng/Missing.h
-	Include/deng/MissingTextureBuilder.h
-	Include/deng/ProgramFilesManager.h
-	Include/deng/RenderResources.h
-	Include/deng/ResourceEvents.h
-	Include/deng/ResourceIdTable.h
-	Include/deng/Scene.h
-	Include/deng/SceneEvents.h
-	Include/deng/SceneRenderer.h
-	Include/deng/SDLWindowContext.h
-	Include/deng/SkyboxBuilders.h
+	Include/deng/IFramebuffer.h
+	Include/deng/GPUMemoryAllocator.h
+)
+
+set(DENG_RENDERER_BACKEND_SOURCES
+	Sources/GPUMemoryAllocator.cpp)
+	
+set(DENG_RENDERER_BACKEND_VULKAN_HEADERS
 	Include/deng/VulkanFramebuffer.h
 	Include/deng/VulkanHelpers.h
 	Include/deng/VulkanInstanceCreator.h
 	Include/deng/VulkanPipelineCreator.h
 	Include/deng/VulkanRenderer.h
-	Include/deng/VulkanSwapchainCreator.h
-	Include/deng/WindowEvents.h)
-	
-set(DENG_MINIMAL_SOURCES
-	Sources/App.cpp
-	Sources/CameraTransformer.cpp
-	Sources/ErrorDefinitions.cpp
-	Sources/FileTextureBuilder.cpp
-	Sources/FileSystemShader.cpp
-	Sources/GPUMemoryAllocator.cpp
-	Sources/ImGuiLayer.cpp
-	Sources/ImGuiResourceBuilders.cpp
-	Sources/IShader.cpp
-	Sources/Missing.cpp
-	Sources/MissingTextureBuilder.cpp
-	Sources/ProgramFilesManager.cpp
-	Sources/Scene.cpp
-	Sources/SceneRenderer.cpp
-	Sources/SDLWindowContext.cpp
-	Sources/Singletons.cpp
-	Sources/SkyboxBuilders.cpp
+	Include/deng/VulkanSwapchainCreator.h)
+
+set(DENG_RENDERER_BACKEND_VULKAN_SOURCES
 	Sources/VulkanFramebuffer.cpp
 	Sources/VulkanHelpers.cpp
 	Sources/VulkanInstanceCreator.cpp
 	Sources/VulkanPipelineCreator.cpp
 	Sources/VulkanRenderer.cpp
 	Sources/VulkanSwapchainCreator.cpp)
+
+set(DENG_SHADER_HEADERS
+	Include/deng/IShader.h
+	Include/deng/FileSystemShader.h)
+
+set(DENG_SHADER_SOURCES
+	Sources/IShader.cpp
+	Sources/FileSystemShader.cpp)
+
+set(DENG_SHADER_COMPILER_HEADERS
+	Include/deng/IGraphicsShaderSpirvCompiler.h)
+
+set(DENG_SHADER_COMPILER_HLSL_HEADERS
+	Include/deng/HLSLGraphicsShaderSpirvCompiler.h)
+
+set(DENG_SHADER_COMPILER_HLSL_SOURCES
+	Sources/HLSLGraphicsShaderSpirvCompiler.cpp)
+
+set(DENG_UTIL_HEADERS
+	Include/deng/CameraTransformer.h
+	Include/deng/ErrorDefinitions.h
+	Include/deng/Exceptions.h
+	Include/deng/FileTextureBuilder.h
+	Include/deng/InitializerListBuilder.h
+	Include/deng/MathConstants.h
+	Include/deng/Missing.h
+	Include/deng/MissingTextureBuilder.h
+	Include/deng/ProgramFilesManager.h
+	Include/deng/SkyboxBuilders.h)
+
+set(DENG_UTIL_SOURCES
+	Sources/CameraTransformer.cpp
+	Sources/ErrorDefinitions.cpp
+	Sources/FileTextureBuilder.cpp
+	Sources/Missing.cpp
+	Sources/MissingTextureBuilder.cpp
+	Sources/ProgramFilesManager.cpp
+	Sources/Singletons.cpp
+	Sources/SkyboxBuilders.cpp)
+
+set(DENG_CORE_HEADERS
+	Include/deng/Api.h
+	Include/deng/App.h
+	Include/deng/ILayer.h
+	Include/deng/RenderResources.h)
+
+set(DENG_CORE_SOURCES
+	Sources/App.cpp)
+
+set(DENG_SCENE_HEADERS
+	Include/deng/Components.h
+	Include/deng/ResourceIdTable.h
+	Include/deng/Scene.h
+	Include/deng/SceneRenderer.h)
+
+set(DENG_SCENE_SOURCES
+	Sources/Scene.cpp
+	Sources/SceneRenderer.cpp)
+
+set(DENG_WINDOW_HEADERS
+	Include/deng/IWindowContext.h)
+
+set(DENG_WINDOW_SOURCES)
+
+set(DENG_WINDOW_SDL_HEADERS
+	Include/deng/SDLWindowContext.h)
+
+set(DENG_WINDOW_SDL_SOURCES
+	Sources/SDLWindowContext.cpp)
+
+set(DENG_EVENT_HEADERS
+	Include/deng/Event.h
+	Include/deng/InputEvents.h
+	Include/deng/ResourceEvents.h
+	Include/deng/SceneEvents.h
+	Include/deng/WindowEvents.h)
 	
+set(DENG_MINIMAL_SOURCES
+	# ImGui
+	${DENG_IMGUI_HEADERS}
+	${DENG_IMGUI_SOURCES}
+	# RendererBackend
+	${DENG_RENDERER_BACKEND_HEADERS}
+	${DENG_RENDERER_BACKEND_SOURCES}
+	# RendererBackend/Vulkan
+	${DENG_RENDERER_BACKEND_VULKAN_HEADERS}
+	${DENG_RENDERER_BACKEND_VULKAN_SOURCES}
+	# Shader
+	${DENG_SHADER_HEADERS}
+	${DENG_SHADER_SOURCES}
+	# Shader/Compiler
+	${DENG_SHADER_COMPILER_HEADERS}
+	# Shader/Compiler/HLSL
+	${DENG_SHADER_COMPILER_HLSL_HEADERS}
+	${DENG_SHADER_COMPILER_HLSL_SOURCES}
+	# Util
+	${DENG_UTIL_HEADERS}
+	${DENG_UTIL_SOURCES}
+	# Core
+	${DENG_CORE_HEADERS}
+	${DENG_CORE_SOURCES}
+	# Scene
+	${DENG_SCENE_HEADERS}
+	${DENG_SCENE_SOURCES}
+	# Window
+	${DENG_WINDOW_HEADERS}
+	${DENG_WINDOW_SOURCES}
+	# Window/SDL
+	${DENG_WINDOW_SDL_HEADERS}
+	${DENG_WINDOW_SDL_SOURCES}
+	# Event
+	${DENG_EVENT_HEADERS})
+
 if (NOT DENG_STATIC)
 	add_library(${DENG_MINIMAL_TARGET} SHARED
-				${DENG_MINIMAL_HEADERS}
-				${DENG_MINIMAL_SOURCES}
-				${DENG_IMGUI_HEADERS}
-				${DENG_IMGUI_SOURCES})
+				${DENG_MINIMAL_SOURCES})
 else()
 	add_library(${DENG_MINIMAL_TARGET} STATIC
-				${DENG_MINIMAL_HEADERS}
-				${DENG_MINIMAL_SOURCES}
-				${DENG_IMGUI_HEADERS}
-				${DENG_IMGUI_SOURCES})
+				${DENG_MINIMAL_SOURCES})
 endif()
 			
 if(WIN32 AND NOT DENG_STATIC)
@@ -155,4 +228,48 @@ target_link_libraries(${DENG_MINIMAL_TARGET}
 	PRIVATE vulkan-1)
 			
 
+source_group(ImGui FILES 
+	${DENG_IMGUI_HEADERS}
+	${DENG_IMGUI_SOURCES})
 
+source_group(RendererBackend FILES
+	${DENG_RENDERER_BACKEND_HEADERS}
+	${DENG_RENDERER_BACKEND_SOURCES})
+
+source_group(RendererBackend/Vulkan FILES
+	${DENG_RENDERER_BACKEND_VULKAN_HEADERS}
+	${DENG_RENDERER_BACKEND_VULKAN_SOURCES})
+
+source_group(Shader FILES
+	${DENG_SHADER_HEADERS}
+	${DENG_SHADER_SOURCES})
+
+source_group(Shader/Compiler FILES
+	${DENG_SHADER_COMPILER_HEADERS})
+
+source_group(Shader/Compiler/HLSL FILES
+	${DENG_SHADER_COMPILER_HLSL_HEADERS}
+	${DENG_SHADER_COMPILER_HLSL_SOURCES})
+
+source_group(Util FILES
+	${DENG_UTIL_HEADERS}
+	${DENG_UTIL_SOURCES})
+
+source_group(Core FILES
+	${DENG_CORE_HEADERS}
+	${DENG_CORE_SOURCES})
+
+source_group(Scene FILES
+	${DENG_SCENE_HEADERS}
+	${DENG_SCENE_SOURCES})
+
+source_group(Window FILES
+	${DENG_WINDOW_HEADERS}
+	${DENG_WINDOW_SOURCES})
+
+source_group(Window/SDL FILES
+	${DENG_WINDOW_SDL_HEADERS}
+	${DENG_WINDOW_SDL_SOURCES})
+
+source_group(Event FILES
+	${DENG_EVENT_HEADERS})
